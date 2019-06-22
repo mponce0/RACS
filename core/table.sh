da076b9bc859e17e0c9cd10e60a0d3724373c74c
Marcelo Ponce <mponce@scinet.utoronto.ca>
2019-06-22 16:17:08 -0400

    making the name of the TMP files include the file to be processed

--- core/table.sh
+++ core/table.sh
@@ -8,7 +8,7 @@ FILE=$1
 
 #####################################################################
 #####################################################################
-## The folllowing two sections:
+## The following two sections:
 ##
 ##		"FILTERS AND DELIMITERS"
 ##
@@ -52,24 +52,29 @@ delim3="Note="
 ################## selection ... #####################################
 
 # grab scafold and genes' range
-grep $filter1 $FILE | grep $filter2 | awk '{print $1" "$4"-"$5}' > tmp0
+grep $filter1 $FILE | grep $filter2 | awk '{print $1" "$4"-"$5}' > tmp0.$FILE
 
 # grab "TTHERM", ie. *delim1*
-# grep $filter1 $FILE | grep $filter2 | awk 'BEGIN{FS="TTHERM"} {print $2}' | awk	'BEGIN{FS=";Note"} {print "TTHERM"$1}' > tmp1
-grep $filter1 $FILE | grep $filter2 | awk  -v d1="$delim1" 'BEGIN{FS=d1} {print $2}' | awk -v d1="$delim1" -v d2="$delim2" 'BEGIN{FS=d2} {print d1$1}' > tmp1
+# grep $filter1 $FILE | grep $filter2 | awk 'BEGIN{FS="TTHERM"} {print $2}' | awk	'BEGIN{FS=";Note"} {print "TTHERM"$1}' > tmp1.$FILE
+grep $filter1 $FILE | grep $filter2 | awk  -v d1="$delim1" 'BEGIN{FS=d1} {print $2}' | awk -v d1="$delim1" -v d2="$delim2" 'BEGIN{FS=d2} {print d1$1}' > tmp1.$FILE
 
 # grab 'Note' and replace 'spaces' with 'underscores (_)'
-#grep $filter1 $FILE | grep $filter2 | awk 'BEGIN{FS="Note="} {print "Note="$2}' | sed 's/ /_/g' > tmp2
-grep $filter1 $FILE | grep $filter2 | awk -v d3="$delim3" 'BEGIN{FS=d3} {print d3$2}' | sed 's/ /_/g' > tmp2
+#grep $filter1 $FILE | grep $filter2 | awk 'BEGIN{FS="Note="} {print "Note="$2}' | sed 's/ /_/g' > tmp2.$FILE
+grep $filter1 $FILE | grep $filter2 | awk -v d3="$delim3" 'BEGIN{FS=d3} {print d3$2}' | sed 's/ /_/g' > tmp2.$FILE
 
 # compute gene size
-grep $filter1 $FILE | grep $filter2 | awk '{print $5-$4+1}' > tmp3
+grep $filter1 $FILE | grep $filter2 | awk '{print $5-$4+1}' > tmp3.$FILE
 
 #######################################################################
 
 # join sliced data (tmp.files) to generate a combined table
-paste tmp0 tmp1 tmp2 tmp3 | sort  -k 1 | sort -t: -n -k 2  | awk '{print $1":"$2"\t"$3"\t"$4"\t"$5"\t"$6}' > table.$FILE
+#paste tmp0 tmp1 tmp2 tmp3
+paste tmp0.$FILE tmp1.$FILE tmp2.$FILE tmp3.$FILE | sort  -k 1 | sort -t: -n -k 2  | awk '{print $1":"$2"\t"$3"\t"$4"\t"$5"\t"$6}' > table.$FILE
 
 # cleanup: remove temporary files...
-rm tmp0 tmp1 tmp2 tmp3
+#rm tmp0 tmp1 tmp2 tmp3
+rm -v tmp?.$FILE
 
+
+#######################################################################
+#######################################################################
