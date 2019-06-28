c2c4d9bf9a066e8ece650b6f000753c1475da982
Marcelo Ponce <mponce@scinet.utoronto.ca>
2019-06-28 14:26:24 -0400

    adding checks to the script that gets the data for the Oxytricha

--- datasets/get_OXYchIPseq-files.sh
+++ datasets/get_OXYchIPseq-files.sh
@@ -4,10 +4,33 @@
 # This script is part of
 # RACS v1.0 (2018/2019) -- Open source tools for Analizing ChIP-Seq data                                   
 
-# It requires the "NSBI SRA toolkit" to download the data
+# It requires the "NCBI SRA (Sequence Read Archive)" to download the data
+# whcioh can be obtained from	https://github.com/ncbi/sra-tools
 
 # set PATH to add "SRA toolkit"
 PATH=$PATH:/scratch1/mponce/RESEARCH/Tetrahymena_Ryerson/TOOLS/NCBI_SRA_toolkit/sratoolkit.2.9.6-1-ubuntu64/bin
+
+#######################################################
+# check that the script is not being sourced!!!
+if [[ $0 == '-bash' ]]; then
+        echo "Please do not 'source' this script; ie. run it as PATHtoRACS/core/SCRIPTname arguments"
+        return 1
+fi
+
+# setting preamble, detecting scripts location
+scriptsDIR=`dirname $0`
+
+
+# load auxiliary fns for integrity checks and message/error handling
+if [[ -f $scriptsDIR/../core/auxs/auxFns.sh ]]; then
+        . $scriptsDIR/../core/auxs/auxFns.sh --source-only;
+else
+        echo "Error auxiliary file: " $scriptsDIR/auxs/auxFns.sh "NOT found!"
+        return 11
+fi
+#######################################################
+
+checkTools fastq-dump gzip
 
 # Oxytricha files:
 srxFiles="SRX483016 SRX483017"
