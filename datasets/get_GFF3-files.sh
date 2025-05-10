c7fe88836c0504452d360e9d755831dd0be10eec
Marcelo P <m.ponce@utoronto.ca>
2025-05-10 11:08:13 -0400

    updating the URLs for datasets retrieval

--- datasets/get_GFF3-files.sh
+++ datasets/get_GFF3-files.sh
@@ -7,23 +7,37 @@
 # Shell script provided to obtain dataset for:
 #
 # Tetrahymena Thermophila
+# 	(legacy)
 #	GFF3:	http://www.ciliate.org/system/downloads/T_thermophila_June2014.gff3
 #	FASTA:	http://www.ciliate.org/system/downloads/T_thermophila_June2014_assembly.fasta
+#	(2025)
+#	GFF3:	https://tet.ciliate.org/common/downloads/tet/legacy/T_thermophila_June2014.gff3
+#	FASTA:	https://tet.ciliate.org/common/downloads/tet/legacy/T_thermophila_June2014_assembly.fasta
+#
 # and
 #
-# Oxytricha Trifallax"
+# Oxytricha Trifallax
+# 	(legacy)
 #	GFF3:	http://oxy.ciliate.org/system/downloads/Oxytricha_trifallax_022112.gff3
 #	FASTA:	http://oxy.ciliate.org/system/downloads/Oxytricha_trifallax_022112_assembly.fasta
+#	(2025)
+#	GFF3:	https://oxy.ciliate.org/common/downloads/oxy/Oxytricha_trifallax_022112.gff3
+#	FASTA:	https://oxy.ciliate.org/common/downloads/oxy/Oxytricha_trifallax_022112_assembly.fasta
 
 
 ###
 # Tetrahymena Thermophila
 TT_gff3_URL="http://www.ciliate.org/system/downloads/T_thermophila_June2014.gff3"
+TT_gff3_URL="https://tet.ciliate.org/common/downloads/tet/legacy/T_thermophila_June2014.gff3"
 TT_fasta_URL="http://www.ciliate.org/system/downloads/T_thermophila_June2014_assembly.fasta"
+TT_fasta_URL="https://tet.ciliate.org/common/downloads/tet/legacy/T_thermophila_June2014_assembly.fasta"
+
 #
 # Oxytricha Trifallax
 OXY_gff3_URL="http://oxy.ciliate.org/system/downloads/Oxytricha_trifallax_022112.gff3"
+OXY_gff3_URL="https://oxy.ciliate.org/common/downloads/oxy/Oxytricha_trifallax_022112.gff3"
 OXY_fasta_URL="http://oxy.ciliate.org/system/downloads/Oxytricha_trifallax_022112_assembly.fasta"
+OXY_fasta_URL="https://oxy.ciliate.org/common/downloads/oxy/Oxytricha_trifallax_022112_assembly.fasta"
 ###
 
 # select tool for downloading data...
